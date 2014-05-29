a7d029211a1e09fccab1b80aa7b78e59877b2f5e
czaks <marcin@6irc.net>
2014-05-29 22:23:37 +0200

    iteracja

--- lib/minifw/template.php
+++ lib/minifw/template.php
@@ -3,7 +3,7 @@ function render($template, $scope) {
   extract($scope);                     // $scope['test'] to teraz $test
   ini_set("short_open_tag", true);     // włączamy krótkie tagi <?, <?=, ? >
   ob_start();                          // rozpoczynamy buforowanie
-  include("templates/$template.php");  // odpalamy szablon
+  include("templates/$template.html"); // odpalamy szablon
   $ret = ob_get_contents();            // łapiemy wyjście z szablonu
   ob_end_clean();                      // czyścimy bufor
   ini_set("short_open_tag", false);    // wyłączamy krótkie tagi <?, <?=, ? >
