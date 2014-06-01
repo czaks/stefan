02e088fb822bdef878c9a8a903890cbe3aca1ab8
czaks <marcin@6irc.net>
2014-06-01 19:34:04 +0200

    notice

--- lib/minifw/template.php
+++ lib/minifw/template.php
@@ -2,7 +2,9 @@
 function render($template, $scope) {
   extract($scope);                     // $scope['test'] to teraz $test
   ob_start();                          // rozpoczynamy buforowanie
+  error_reporting(E_ALL ^ E_NOTICE);   // nie pokazuj noticów
   include("templates/$template.html"); // odpalamy szablon
+  error_reporting(E_ALL);              // włącz je spowrotem
   $ret = ob_get_contents();            // łapiemy wyjście z szablonu
   ob_end_clean();                      // czyścimy bufor
   return $ret;                         // zwracamy to co nam szablon oddał
